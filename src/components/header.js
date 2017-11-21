0993a6a872ab4c373acfd65085ec16915117383c
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-21 15:56:03 -0800

    create component for style to change the header font style and use curly brackets around const Header and return

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,15 @@
 import React from 'react';
 import { Text } from 'react-native';
 // Make a component
-const Header = () => (
-  <Text> Albums! </Text>
-);
+const Header = () => {
+  const { textStyle } = styles;
+  return <Text style={textStyle}> Albums! </Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
 // Make the component available to other parts of the app
 export default Header;
