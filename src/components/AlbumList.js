54e9daa40a9babd67c1ee66015a0338983d95af5
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 10:20:32 -0800

    Input component will mount into Album list Class component to fetch data and also

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 class AlbumList extends Component {
+componentWillMount() {
+  console.log('compnentWillMount in AlbumList');
+}
+
   render() {
     return (
       <View>
