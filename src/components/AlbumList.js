7ff9a31c4124555da8fdfe975af9b5eef9edf23f
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 11:47:06 -0800

    Set state of retrieved data to prepare for rendering

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -7,10 +7,11 @@ class AlbumList extends Component {
 
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({ albums: response.data }));
   }
 
   render() {
+    console.log(this.state);
     return (
       <View>
         <Text> Album List !!!</Text>
