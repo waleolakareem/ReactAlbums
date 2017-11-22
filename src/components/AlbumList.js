a294a7623b35155f5e08b47146f73db10f63a64c
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 11:16:15 -0800

    Step one of rendering state before component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,6 +3,8 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => console.log(response));
