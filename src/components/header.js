3bc4d75c3c53c240d94dd92565be637acb552c1c
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-21 16:34:24 -0800

    Wrapped View component  around text component to enable center tyling of header text

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,14 @@
 // Import Libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 // Make a component
 const Header = () => {
   const { textStyle } = styles;
-  return <Text style={textStyle}> Albums! </Text>;
+  return (
+    <View>
+      <Text style={textStyle}> Albums! </Text>
+    </View>
+    );
 };
 
 const styles = {
