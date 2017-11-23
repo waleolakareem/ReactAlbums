1ad9cb4cbde0d9302ba1e3a470fb2123463bb142
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-23 14:09:49 -0800

    Style image with imageStyle to show image picture and delete View component from image section

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,23 +5,31 @@ import CardSection from './CardSection';
 
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = album;
     const { headerContentStyle,
             thumbnailStyle,
             thumbnailContainerStyle,
-            headerTextStyle } = styles;
+            headerTextStyle,
+            imageStyle } = styles;
   return (
     <Card>
       <CardSection>
         <View style={thumbnailContainerStyle}>
           <Image
           style={thumbnailStyle}
-          source={{ uri: thumbnail_image }} />
+          source={{ uri: thumbnail_image }}
+          />
         </View>
         <View style={headerContentStyle}>
           <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+          <Image
+          style={imageStyle}
+          source={{ uri: image }}
+          />
       </CardSection>
     </Card>
   );
@@ -48,6 +56,12 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 export default AlbumDetail;
