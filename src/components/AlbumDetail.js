152592da7a15cbe4e29956b5736186a79e56ead9
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 18:07:38 -0800

    thumbnailContainer property to style View component containing artist image,name and title 👩‍🚒

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -34,6 +34,13 @@ const styles = {
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 export default AlbumDetail;
