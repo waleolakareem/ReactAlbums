2b72e77dd2903c7a0034d0dc91bd275fdc8962ec
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 16:03:04 -0800

    Called card component inside AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Card from './Card';
 
 
 const AlbumDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   );
 };
 
