4ee8bd13e8fcbdb526db1640fc797bc138c1721a
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-22 16:09:43 -0800

    Passed prop into card to render any thing rendered within it with props.c

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <View style={styles.containerStyle}></View>
+    <View style={styles.containerStyle}>
+      {props.children}
+    </View>
   );
 };
 
