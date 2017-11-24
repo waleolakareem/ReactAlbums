bb35b55bbf710f409ad0aa4639da369ccc2db5db
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-23 18:38:08 -0800

    Direct users to uri to buy album on click

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { headerContentStyle,
             thumbnailStyle,
             thumbnailContainerStyle,
@@ -35,7 +35,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => console.log(title)} />
+        <Button onPress={() => Linking.openURL(url)} />
       </CardSection>
     </Card>
   );
