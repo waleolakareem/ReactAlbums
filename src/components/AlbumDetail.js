b5447527e577ba7da5f846534a0abede858a886c
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-23 18:50:18 -0800

    Made button to be reusable any where by passing prop from album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -35,7 +35,9 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button onPress={() => Linking.openURL(url)} />
+        <Button onPress={() => Linking.openURL(url)}>
+          Buy Now
+        </Button>
       </CardSection>
     </Card>
   );
